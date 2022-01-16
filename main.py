0b39c88f71b8b8da4c7087120a8c6f4ed7f03d05
Reboot93 <seerseer93@gmail.com>
2022-01-17 00:24:38 +0800

    未完成

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ files_out_dir = ''
 
 input_files_dict = {}
 input_sub_dict = {}
+out_dict = {}
 
+mission_all = 0
+mission_now = 0
+
+command = ''
 
 class MainWindow(QWidget, Ui_Form):
 
@@ -25,20 +30,28 @@ class MainWindow(QWidget, Ui_Form):
         self.pushButton_files_add.clicked.connect(lambda: self.input_files_add())
         self.pushButton_files_del_one.clicked.connect(lambda: self.input_files_del_one())
         self.pushButton_files_del_all.clicked.connect(lambda: self.input_files_del_all())
+        self.pushButton_files_up.clicked.connect(lambda: self.input_files_up())
+        self.pushButton_files_down.clicked.connect(lambda: self.input_files_down())
         self.pushButton_files_sub_add.clicked.connect(lambda: self.input_sub_add())
         self.pushButton_files_sub_del_one.clicked.connect(lambda: self.input_sub_del_one())
         self.pushButton_files_sub_del_all.clicked.connect(lambda: self.input_sub_del_all())
         self.pushButton_files_sub_pass.clicked.connect(lambda: self.input_sub_pass())
+        self.pushButton_files_sub_up.clicked.connect(lambda: self.input_sub_up())
+        self.pushButton_files_sub_down.clicked.connect(lambda: self.input_sub_down())
         self.pushButton_out_dir.clicked.connect(lambda: self.set_files_out_dir())
         self.pushButton_start.clicked.connect(lambda: self.start())
         self.pushButton_stop.clicked.connect(lambda: self.stop())
 
         # ==/ 界面初始化 /==========
         # self.show_potdir.setText(pot_dir)
+        self.label_num_all.setText(str(mission_all))
+        self.label_num_now.setText(str(mission_now))
+        self.progressBar.setRange(0, 10)
         # 界面更新
         _thread.start_new_thread(lambda: self.flash(), ())
 
     def flash(self):  # ==/ 页面刷新函数 /====
+        pass
         # ===/ 更新控件 /=================================
         self.lineEdit_ffmpeg_dir.setText(ffmpeg_dir)
         self.lineEdit__out_dir.setText(files_out_dir)
@@ -71,7 +84,6 @@ class MainWindow(QWidget, Ui_Form):
                 filename = file[file.rfind('/') + 1:]
                 input_files_dict[filename] = file
                 self.listWidget_files_input.addItem(filename)
-            self.flash()
 
     def input_files_del_one(self):
         try:
@@ -80,7 +92,6 @@ class MainWindow(QWidget, Ui_Form):
             del input_files_dict[file]
         except:
             print('error')
-        self.flash()
 
     def input_files_del_all(self):
         try:
@@ -88,7 +99,28 @@ class MainWindow(QWidget, Ui_Form):
             input_files_dict.clear()
         except:
             print('error')
-        self.flash()
+
+    def input_files_up(self):
+        try:
+            count = self.listWidget_files_input.currentRow()
+            uptext = self.listWidget_files_input.currentItem().text()
+            self.listWidget_files_input.takeItem(count)
+            self.listWidget_files_input.insertItem(count - 1, uptext)
+            if self.listWidget_files_input.currentRow() > 0:
+                self.listWidget_files_input.setCurrentRow(count - 1)
+        except:
+            print('error')
+
+    def input_files_down(self):
+        try:
+            count = self.listWidget_files_input.currentRow()
+            downtext = self.listWidget_files_input.currentItem().text()
+            self.listWidget_files_input.takeItem(count)
+            self.listWidget_files_input.insertItem(count + 1, downtext)
+            if self.listWidget_files_input.currentRow() < self.listWidget_files_input.count():
+                self.listWidget_files_input.setCurrentRow(count + 1)
+        except:
+            print('error')
 
     def input_sub_add(self):
         subs = QFileDialog.getOpenFileNames(self, '选择需要内嵌得字幕文件')
@@ -98,7 +130,6 @@ class MainWindow(QWidget, Ui_Form):
                 subname = sub[sub.rfind('/') + 1:]
                 input_sub_dict[subname] = sub
                 self.listWidget_sub.addItem(subname)
-            self.flash()
 
     def input_sub_del_one(self):
         try:
@@ -107,7 +138,6 @@ class MainWindow(QWidget, Ui_Form):
             del input_sub_dict[sub]
         except:
             print('error')
-        self.flash()
 
     def input_sub_del_all(self):
         try:
@@ -115,13 +145,76 @@ class MainWindow(QWidget, Ui_Form):
             input_sub_dict.clear()
         except:
             print('error')
-        self.flash()
 
     def input_sub_pass(self):
-        pass
+        count = self.listWidget_sub.currentRow()
+        self.listWidget_sub.insertItem(count, '此视频不添加内嵌字幕')
+
+    def input_sub_up(self):
+        try:
+            count = self.listWidget_sub.currentRow()
+            uptext = self.listWidget_sub.currentItem().text()
+            self.listWidget_sub.takeItem(count)
+            self.listWidget_sub.insertItem(count - 1, uptext)
+            if self.listWidget_sub.currentRow() > 0:
+                self.listWidget_sub.setCurrentRow(count - 1)
+        except:
+            print('error')
+
+    def input_sub_down(self):
+        try:
+            count = self.listWidget_sub.currentRow()
+            downtext = self.listWidget_sub.currentItem().text()
+            self.listWidget_sub.takeItem(count)
+            self.listWidget_sub.insertItem(count + 1, downtext)
+            if self.listWidget_sub.currentRow() < self.listWidget_sub.count():
+                self.listWidget_sub.setCurrentRow(count + 1)
+        except:
+            print('error')
 
     def start(self):
-        pass
+        self.pushButton_start.setEnabled(False)
+        if self.listWidget_files_input.count() == 0 :
+            self.pushButton_start.setEnabled(True)
+        else:
+            file_count = self.listWidget_files_input.count()
+            if self.groupBox_encode.clicked:
+                if self.radioButton_encode_libx264.isChecked():
+                    radio = ' -c:v libx264'
+                elif self.radioButton_encode_libx265.isChecked():
+                    radio = ' -c:v libx265'
+                elif self.radioButton_encode_h264_nvenc.isChecked():
+                    radio = ' -c:v h264_nvenc'
+                elif self.radioButton_encode_hevc_nvenc.isChecked():
+                    radio = ' -c:v hevc_nvenc'
+                elif self.radioButton_encode_cust.isChecked():
+                    radio = self.lineEdit_encode_cust.text()
+                encode = radio
+            else:
+                encode = ''
+            if self.groupBox_rate.clicked:
+                if self.radioButton_rate_rate.isChecked():
+                    bv = ' -b:v %s' % self.lineEdit_rate_rate.text()
+                else:
+                    bv = ''
+                if self.radioButton_rate_bufsize.isChecked():
+                    buf = ' -bufsize %s' % self.lineEdit_rate_bufsize.text()
+                else:
+                    buf = ''
+                if self.radioButton_rate_max.isChecked():
+                    max = ' -maxrate %s' % self.lineEdit_rate_max.text()
+                else:
+                    max = ''
+                if self.radioButton_rate_cust.isChecked():
+                    cust = ' %s' % self.lineEdit_rate_cust.text()
+                else:
+                    cust = ''
+                rate = bv + buf + max + cust
+            else:
+                rate = ''
+            #for number in range(file_count):
+            #    filename = self.listWidget_files_input.item().text()
+
 
     def stop(self):
         pass
